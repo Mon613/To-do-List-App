838a80afd50f2b0be00533248abcf89bf446768e
Mon613 <nguyenha61314@gmail.com>
2025-02-25 23:48:02 +0700

    Added my project

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", loadTasks);
 
 function loadTasks() {
-    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
     tasks.forEach(task => renderTask(task.text, task.done));
 }
 
 function renderTask(taskText, done = false) {
-    const li = document.createElement("li");
-    const span = document.createElement("span");
+    let li = document.createElement("li");
+    let span = document.createElement("span");
     span.innerText = taskText;
     span.contentEditable = true;
     span.addEventListener("input", saveTasks);
     
     if (done) li.classList.add("done");
     
-    const doneBtn = document.createElement("button");
+    let doneBtn = document.createElement("button");
     doneBtn.innerText = "Done";
     doneBtn.onclick = () => toggleTask(li);
     
-    const undoneBtn = document.createElement("button");
+    let undoneBtn = document.createElement("button");
     undoneBtn.innerText = "Undone";
     undoneBtn.onclick = () => unDone(li);
 
-    const deleteBtn = document.createElement("button");
+    let deleteBtn = document.createElement("button");
     deleteBtn.innerText = "Delete";
     deleteBtn.onclick = () => deleteTask(li);
     
@@ -31,7 +31,7 @@ function renderTask(taskText, done = false) {
 }
 
 function saveTasks() {
-    const tasks = [...document.querySelectorAll("li")].map(li => ({
+    let tasks = [...document.querySelectorAll("li")].map(li => ({
         text: li.querySelector("span").innerText,
         done: li.classList.contains("done")
     }));
@@ -44,7 +44,7 @@ function deleteTask(li) {
 }
 
 function createTask() {
-    const input = document.getElementById("taskName");
+    let input = document.getElementById("taskName");
     if (input.value.trim()) {
         renderTask(input.value);
         saveTasks();
