a9f6d165566d2964b0374926225da9c574de0701
Mon613 <nguyenha61314@gmail.com>
2025-02-26 10:13:26 +0700

    Added my project

--- index2.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", loadTasks);
-
-function loadTasks2(){
-    let tasks = JSON.parse(localStorage.getItem("tasks"))||[];
-    tasks.forEach(task => renderTask(task.text,task.cone))
-}
